f75d17ddf884a2050721145b2d79a3d9c48554d5
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-12-24 22:25:55 +0000

    Update to JUnit 4

--- app/src/main/java/com/uk/jacob/containerdroid/activities/MainActivity.java
+++ app/src/main/java/com/uk/jacob/containerdroid/activities/MainActivity.java
@@ -1,6 +1,6 @@
 package com.uk.jacob.containerdroid.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,7 +9,7 @@ import com.uk.jacob.containerdroid.services.CAdvisorService;
 
 import com.uk.jacob.containerdroid.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
